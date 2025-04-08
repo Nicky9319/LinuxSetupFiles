85d834e17bbca704381b75b10a1701ea7a6a5115
Nicky9319 <paarthsaxena2005@gmail.com>
2025-04-09 03:57:35 +0530

    Added the Sh File to Install and Configure google chrome in linux

---
+++ google_chrome.sh
@@ -0,0 +1,7 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg-deb -x google-chrome-stable_current_amd64.deb /path/to/your/directory
+
+sudo apt update
+sudo apt install libatk1.0-0
+sudo apt install libxkbcommon0
+sudo apt install libcairo2
