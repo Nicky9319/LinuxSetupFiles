6e7d70e6fb0f8c60b841d50b33af231c7c3c0823
Paarth Saxena <102816440+Nicky9319@users.noreply.github.com>
2025-04-21 06:59:36 +0530

    Update python3.12_Setup.sh

--- python3.12_Setup.sh
+++ python3.12_Setup.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt install software-properties-common -y # This is Needed When the add-apt-repository is Not Added to the Instance By Default
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install python3.12 python3.12-venv -y
